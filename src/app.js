45852c68a956c4a2ffe689baef0552fd1f894a40
Demonic30 <koffareza007@gmail.com>
2018-11-15 10:05:37 +0700

    get Petname

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
   res.render('index', {
     name: "Coffaree Hahza",
-    pets: ["Stamp", "Snow"]
+    pets: ["KoKo", "Miky"]
   });
 });
 
